8eadb356542b68a56282ce6c9c60bdfeb28c13c1
Anup Agarwal <108anup@gmail.com>
2016-10-15 14:44:20 +0530

    Update crack_htpasswd.py

--- crack_htpasswd.py
+++ crack_htpasswd.py
@@ -5,20 +5,18 @@ from sys import argv
 from crypt import crypt
 
 def generate_strings(length, s):
-
-	if(not found):
-		if(length == 0):
-			#print s;
-			if(crypt(s,salt)[len(salt):]==chckstring):
-				global found 
-				found = True
-				global cracked
-				cracked = s
-		
-		else:
-			for i in range(0,26):
-				app_s = s+alphabets[i]
-				generate_strings(length-1,app_s);
+    if(not found):
+        if(length == 0):
+            #print s;
+            if(crypt(s,salt)[len(salt):]==chckstring):
+                global found 
+                found = True
+                global cracked
+                cracked = s
+        else:
+            for i in range(0,26):
+                app_s = s+alphabets[i]
+                generate_strings(length-1,app_s);
 				
 
 alphabets = ['a',  'b',  'c',  'd',  'e',  'f',  'g',  'h',  'i',  'j',  'k',  'l',  'm',  'n',  'o',  'p',  'q',  'r',  's',  't',  'u',  'v',  'w',  'x',  'y',  'z']
@@ -28,8 +26,8 @@ cracked = ""
 salt = argv[2]	
 i = 1	
 while(not found):
-	print "Attempting with length = ",i
-	generate_strings(i,"")
-	i+=1
+    print "Attempting with length = ",i
+    generate_strings(i,"")
+    i+=1
 else:
-	print chckstring,cracked
+    print chckstring,cracked
